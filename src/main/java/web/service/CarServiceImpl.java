a1d6e1f3625ff58e07412d7c562fb3b0bd82520f
ValSipkin <valsipkin@icloud.com>
2022-11-23 20:26:32 +0300

    2.2.2 решение 2

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -7,14 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 @Service
 public class CarServiceImpl implements CarService{
-    @Override
-    public List<Car> getCars(int carsNumber) {
-        List<Car> carList = new ArrayList<>();
+    private List<Car> carList;
+
+    public CarServiceImpl() {
+        carList = new ArrayList<>();
         carList.add(new Car("BMW", "black", 777));
         carList.add(new Car("Ford", "grey", 345));
         carList.add(new Car("Toyota", "gold", 222));
         carList.add(new Car("Mercedes-Benz", "black", 964));
         carList.add(new Car("Audi", "white", 117));
+    }
+
+    @Override
+    public List<Car> getCars(int carsNumber) {
         if (carsNumber > 4) {
             return carList;
         }
